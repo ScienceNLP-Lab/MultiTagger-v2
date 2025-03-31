bded6ddcb2b17e8c44881d2ebe5d07c122d84cc0
Joe Menke <143003311+jomenke@users.noreply.github.com>
2025-03-31 09:46:20 -0500

    add shell script for baseline exp

--- JBI/experiments/exp_v0_amia_baseline.sh
+++ JBI/experiments/exp_v0_amia_baseline.sh
@@ -3,11 +3,11 @@
 #echo commands to stdout
 set -x
 
-cd /ocean/projects/cis230089p/jmenke/multitagger_v3
+cd /path/to/project/dir
 
 # activate multitagger environment
-source /jet/home/jmenke/miniconda3/bin/activate multitagger
+source /username/miniconda3/bin/activate your_virtual_env
 
-# Model - smallest training set; all features w/ feature verbalization
+# Model - baseline model using architecture and features from AMIA work
 python train.py --experiment_name="00_amia_baseline" --bert_model_name="microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext" --verbalize="original" --loss_function="bce" --lr_scheduler="linear" --optimizer="AdamW"
 
